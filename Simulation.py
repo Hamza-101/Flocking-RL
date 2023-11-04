6bd88c590a935095d74b0f72fa268af947c934f0
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2023-11-04 20:21:14 +0500

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -17,79 +17,18 @@ class Agent:
         self.max_acceleration = SimulationVariables["AccelerationUpperLimit"]
         self.max_velocity = SimulationVariables["VelocityUpperLimit"]
 
-    def update(self, agents, dt):
-        positions = np.array([agent.position for agent in agents])
-        velocities = np.array([agent.velocity for agent in agents])
-
-        neighbor_indices = self.get_closest_neighbors(agents, SimulationVariables["NeighborhoodRadius"])
-        self.flock(neighbor_indices, positions, velocities)
-        self.velocity += self.acceleration * dt
+    def update(self, dt, action):
+        self.velocity += action
 
         vel = np.linalg.norm(self.velocity)
         if vel > self.max_velocity:
             self.velocity = (self.velocity / vel) * self.max_velocity
 
         self.position += self.velocity * dt
+        print(self.position)
 
-    def flock(self, neighbor_indices, positions, velocities):
-        alignment = self.align(neighbor_indices, velocities)
-        cohesion = self.cohere(neighbor_indices, positions)
-        separation = self.separate(neighbor_indices, positions)
+        return self.position
 
-        total_force = (
-            ((ReynoldsVariables["w_alignment"]) * alignment) +
-            ((ReynoldsVariables["w_cohesion"]) * cohesion) +
-            ((ReynoldsVariables["w_separation"]) * separation)
-        )
-
-        self.acceleration = np.clip(total_force, 0, self.max_acceleration)
-
-    # def align(self, neighbor_indices, velocities):
-    #     if len(neighbor_indices) > 0:
-    #         neighbor_velocities = velocities[neighbor_indices]
-    #         average_velocity = np.mean(neighbor_velocities, axis=0)
-    #         desired_velocity = average_velocity - self.velocity
-    #         return desired_velocity
-    #     else:
-    #         return np.zeros(2)
-
-    # def cohere(self, neighbor_indices, positions):
-    #     if len(neighbor_indices) > 0:
-    #         neighbor_positions = positions[neighbor_indices]
-    #         center_of_mass = np.mean(neighbor_positions, axis=0)
-    #         desired_direction = center_of_mass - self.position
-    #         return desired_direction
-    #     else:
-    #         return np.zeros(2)
-
-    # def separate(self, neighbor_indices, positions):
-    #     if len(neighbor_indices) > 0:
-    #         neighbor_positions = positions[neighbor_indices]
-    #         separation_force = np.zeros(2)
-
-    #         for neighbor_position in neighbor_positions:
-    #             relative_position = self.position - neighbor_position
-    #             distance = np.linalg.norm(relative_position)
-
-    #             if distance > 0:
-    #                 separation_force += (relative_position / (distance * distance))
-
-    #         return separation_force
-    #     else:
-    #         return np.zeros(2)
-
-    # def get_closest_neighbors(self, agents, max_distance):
-    #     neighbor_indices = []
-
-    #     for i, agent in enumerate(agents):
-    #         if agent != self:
-    #             distance = np.linalg.norm(agent.position - self.position)
-    #             if distance < max_distance:
-    #                 neighbor_indices.append(i)
-
-    #     return neighbor_indices 
-    
-    
 class Encoder(json.JSONEncoder):
         def default(self, obj):
             return json.JSONEncoder.default(self, obj)
@@ -101,21 +40,22 @@ class FlockingEnv(gym.Env):
         self.flocking_agents = agents
 
         #Check what to use velocity and or acceleration [5, 5]
-        min_obs = np.array([[-np.inf, -np.inf]] * len(self.flocking_agents), dtype=np.float32)
-        max_obs = np.array([[np.inf, np.inf]] * len(self.flocking_agents), dtype=np.float32)
+        min_obs = np.array([-np.inf, -np.inf] * len(self.flocking_agents), dtype=np.float32)
+        max_obs = np.array([np.inf, np.inf] * len(self.flocking_agents), dtype=np.float32)
         self.observation_space = spaces.Box(low=min_obs, high=max_obs, dtype=np.float32)
 
-        #
+        
         min_action = np.array([[-5, -5]] * len(self.flocking_agents), dtype=np.float32)
         max_action = np.array([[5, 5]] * len(self.flocking_agents), dtype=np.float32)
         self.action_space = spaces.Box(low=min_action, high=max_action, dtype=np.float32)  # Control over velocity in x and y axes
     
-        print("Action space: ", self.action_space)
-        print("Observation space: ", self.observation_space)
+        print("Action space: ", self.action_space.shape)
+        print("Observation space: ", self.observation_space.shape)
     
     def step(self, actions): 
-        done, observations = simulate_agents(actions)
-        print(observations)
+        print("Actions Before Simulation: ", actions)
+        new_actions=self.action_mapping(actions)
+        done, observations = simulate_agents(new_actions)
         reward=self.calculate_reward()
 
         print("Code.Observations: ", observations)
@@ -181,6 +121,15 @@ class FlockingEnv(gym.Env):
 
         return future_positions, future_velocities
     
+    def action_mapping(actions):
+        velocities = []
+        for action in actions:  
+            action_step_x = action[0] / 10.0
+            action_step_y = action[1] / 10.0
+            velocities.append(np.concatenate(np.array([action_step_x, action_step_y])))
+
+        return velocities
+
     # def remove_fatality(self):
  
 
@@ -188,28 +137,24 @@ def read_agent_locations():
     with open(rf"{Results['InitPositions']}\config.json", "r") as f:
         data = json.load(f)
         print("File loaded")
+        print("Agent Locations: ", data)
     return data
 
-def simulate_agents(agents):
-    agent_data = {id(agent): [] for agent in agents}
-
-    for agent in agents:
-            # agent_id = id(agent)
-            agent.update(agents, SimulationVariables["dt"])
-
-            # agent_data_full = {
-            #     "x": round(agent.position[0], 2),
-            #     "y": round(agent.position[1], 2)
-            # }
-
-            # agent_data[agent_id].append(agent_data_full)
+def simulate_agents(actions):
+    print(actions)
+    observations = []
+    for i, agent in enumerate(agents):
+        # agent_id = id(agent_data)
+        print(f"Agent {i} Position Before Update: {agent.position}")
+        new_position = agent.update(SimulationVariables["dt"], actions[i])
+        print(f"Agent {i} Position After Update: {new_position}")
+        observations.append(new_position)
 
     print("Simulation Complete")
-
+    return observations
 
 agent_locations = read_agent_locations()
 agents = [Agent(position) for position in agent_locations]
-
 
 env = DummyVecEnv([lambda: FlockingEnv(agents)])
 
@@ -217,6 +162,7 @@ actions = np.array([])
 observations = np.array([]) 
 rewards = np.array([])
 
-# model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_Agents_tensorboard/")
-# model.learn(total_timesteps=SimulationVariables["TimeSteps"])
-# model.save("PPO")
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_Agents_tensorboard/")
+model.learn(total_timesteps=SimulationVariables["TimeSteps"])
+model.save("PPO")
+
