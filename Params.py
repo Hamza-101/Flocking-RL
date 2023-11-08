9352d17bd086fd9c0abd7ac9541d3e6544482d7a
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2023-11-08 23:21:57 +0500

    Update Params.py

--- Params.py
+++ Params.py
@@ -1,28 +1,24 @@
 SimulationVariables = {
-    "SimAgents" : 20,
-    "AgentData" : [],
-    "SafetyRadius" : 2, 
-    "NeighborhoodRadius" : 10,
-    "VelocityUpperLimit" : 5.0,
-    "VelocityInit" : 1.0,
-    "AccelerationUpperLimit" : 1.0,
+    "SimAgents": 20,
+    "AgentData": [],
+    "SafetyRadius": 2, 
+    "NeighborhoodRadius": 10,
+    "VelocityUpperLimit": 5.0,
+    "VelocityInit": 1.0,
+    "AccelerationUpperLimit": 1.0,
     "AccelerationInit": 0.0,
     "dt" : 0.1,
-    "TimeSteps" : 1000,
+    "TimeSteps": 1000,
+    "Runtime": 100,
     "X" : 10,
-    "Y" : 10,
-    "Reward" : "", # Check this
-    "TrainFile" : "Simulations\Config_0",
-    "TestFile" : "Simulations\Test"
+    "Y" : 10
 }
 
 Results = {
-    "Directory" : "Simulations",
-    "Sim" : "Simulation", # Change this
-    "InitPositions" : "Simulations\Config_2",
-    "FinalPositions" : "", # Change this
-    "SimDetails" : SimulationVariables,
-    "Algorithm" : "", # Change this
-    "TotalReward": [] # Sum Array when needed
+    "Directory": "Simulations",
+    "Sim": "Simulation",
+    "InitPositions": "Simulations\Config_0",
+    "Positions": "agent_positions.json",
+    "FinalPositions": "",
+    "SimDetails": SimulationVariables
 }
-
